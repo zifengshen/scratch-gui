7973d2abab12903e6bafc8fcc52a9e9f24792040
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2017-06-15 17:42:39 -0400

    Add “work in progress” to title

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -23,7 +23,7 @@ const MenuBar = function MenuBar () {
             </div>
             <SaveButton className={styles.menuItem} />
             <LoadButton className={styles.menuItem} />
-            <div className={classNames(styles.title, styles.menuItem)}>Extensions Prototype</div>
+            <div className={classNames(styles.title, styles.menuItem)}>Extensions Prototype - Work in Progress</div>
         </Box>
     );
 };
